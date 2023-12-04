c0fd2992ffe85b9ae32ee020a5527ba9e49b39b9
sarasamhita <91931605+sarasamhita@users.noreply.github.com>
2023-12-04 09:11:12 +0700

    modul5

---
+++ MODUL5-KIRANA/app/Http/Controllers/ShowroomController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Showroom;
+use Illuminate\Http\Request;
+
+class ShowroomController extends Controller
+{
+  public function index()
+  {
+    $showroom = Showroom::all();
+    return view('showroom/index', compact('Showroom'));
+  }
+
+  public function create()
+  {
+    return view('showroom/create');
+  }
+
+  public function store(Request $request)
+  {
+    $data = $request->all();
+
+    Mobil::create([
+      'nama_mobil' => $data['name'],
+      'brand_mobil' => $data['brand'],
+      'warna_mobil' => $data['warna'],
+      'tipe_mobil' => $data['tipe'],
+      'harga_mobil' => $data['harga'],
+    ]);
+
+    return redirect(route('showroom.index'))->with('success', "Mobil {$data['name']} berhasil ditambahkan");
+  }
+}
